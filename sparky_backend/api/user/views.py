2a60dd6e0404564d9ed7f2b6c756c061f6a3f34c
AbbasAnosh <abbasalianosh@gmail.com>
2023-12-06 11:30:19 -0700

    first commit

---
+++ sparky_backend/api/user/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+import random
+
+
+def generate_session_token(length = 10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
